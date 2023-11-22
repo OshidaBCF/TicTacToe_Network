a030883df87ba75c6238701f76f1e767653a99e1
OshidaBCF <swann.sanchez9@outlook.fr>
2023-11-22 15:41:23 +0100

    changed colowor

--- Zone.cpp
+++ Zone.cpp
@@ -20,7 +20,7 @@ void zone::DrawCircle(sf::RenderWindow *window)
 	sf::CircleShape shape(100);
 	shape.setFillColor(sf::Color(0, 0, 0));
 	shape.setOutlineThickness(-10);
-	shape.setOutlineColor(sf::Color(52, 152, 219));
+	shape.setOutlineColor(sf::Color(255, 255, 255));
 	shape.setPosition(coordinates + sf::Vector2f(50, 50));
 	window->draw(shape);
 }
@@ -28,7 +28,7 @@ void zone::DrawCircle(sf::RenderWindow *window)
 void zone::DrawCross(sf::RenderWindow *window)
 {
 	sf::RectangleShape line(sf::Vector2f(300, 10));
-	line.setFillColor(sf::Color(241, 196, 15));
+	line.setFillColor(sf::Color(255, 255, 255));
 	line.setPosition(coordinates + sf::Vector2f(125.0 / 2.84 + 3.5, 125.0 / 2.84 - 3.5));
 	line.rotate(45);
 	window->draw(line);
